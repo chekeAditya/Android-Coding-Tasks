01bb8a889a8e844b3f20f51f5a28ebab641fec7d
Aditya Cheke <adityacheke4@gmail.com>
2021-10-18 15:39:59 +0530

    mainactivity updation

--- 2.CodingAssingment_Unit-5/app/src/main/java/com/example/unit_5assignment/ui/MainActivity.kt
+++ 2.CodingAssingment_Unit-5/app/src/main/java/com/example/unit_5assignment/ui/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.unit_5assignment.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.unit_5assignment.R
+import com.example.unit_5assignment.databinding.ActivityMainBinding
+import com.example.unit_5assignment.databinding.ItemLayoutBinding
+import com.example.unit_5assignment.ui.adapter.TvMazeAdapter
+import com.example.unit_5assignment.viewmodels.PagingViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var pagingViewModel: PagingViewModel
+    private lateinit var pagingAdapter: TvMazeAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        activityMainBinding.apply {
+
+        }
+        pagingViewModel = ViewModelProvider(this).get(PagingViewModel::class.java)
+        setAdapter()
+    }
+
+    private fun setAdapter() {
+        pagingAdapter = TvMazeAdapter()
+
     }
 }
--- 2.CodingAssingment_Unit-5/app/src/main/java/com/example/unit_5assignment/viewmodels/PagingViewModel.kt
+++ 2.CodingAssingment_Unit-5/app/src/main/java/com/example/unit_5assignment/viewmodels/PagingViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.unit_5assignment.viewmodels
 
-class PagingViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.unit_5assignment.repositorys.PagingRepo
+
+class PagingViewModel : ViewModel() {
+
+    private val pagingRepo = PagingRepo()
+
+    fun searchTvMaje() = pagingRepo.getPagesIst()
+
 }
